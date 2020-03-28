ffab750e1de66191347e73508d7d494a019e5d5b
Aditya Chempakasseril <adityac@live.com>
2020-03-28 02:50:34 -0700

    started express project

--- plots.js
+++ plots.js
@@ -1,5 +1,9 @@
 TESTER = document.getElementById('tester');
-
+// fetch("confirmed_num_cases.json")
+//     .then(response => response.json())
+    //.then(json => console.log(json))
+var obj = require('./confirmed_num_cases.json');
+console.log(obj['China']);
 Plotly.plot( TESTER, [{
     x: [1,2,3,4,5],
     y: [1,2,4,8,16] }], {
